84505e538c518ea149db8c2b87e0a668ec42785f
ssssshyu53 <39176444+ssssshyu53@users.noreply.github.com>
2025-05-03 06:55:27 +0300

    Update telegram_forward_bot.py

--- telegram_forward_bot.py
+++ telegram_forward_bot.py
@@ -7,8 +7,8 @@ api_hash = "678ea1fd4e406db179f0d1ca307e81a7"
 bot_token = "7993866113:AAGyU45CV7_qbjXlQY85Obqk-gRNCiec4M0"
 
 # القنوات (قم بتعديلها بوضع معرف القناة مسبوقًا بـ @)
-source_channel = "@source_channel_username"  # قناة المصدر
-target_channel = "@target_channel_username"  # قناة الهدف
+source_channel = "@YastaftwonkMen3"  # قناة المصدر
+target_channel = "@fatwaWahidbaly"  # قناة الهدف
 
 app = Client("forwarder_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
